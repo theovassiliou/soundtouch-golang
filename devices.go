f852673a4e32e7835b89ed1965321b044f95292b
Theofanis Vassiliou-Gioles <vassiliou@web.de>
2020-06-07 21:35:11 +0200

    encapsulated searching for devices

---
+++ devices.go
@@ -0,0 +1,111 @@
+package soundtouch
+
+import (
+	"net"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type speakers map[string]*Speaker
+type speakerMap map[string]bool
+
+var visibleSpeakers = make(speakers)
+var filteredSpeakers speakerMap
+
+type NetworkConfig struct {
+	InterfaceName      string
+	NoOfSystems        int
+	SpeakerToListenFor []string
+	UpdateHandler      UpdateHandlerFunc
+}
+
+func GetDevices(conf NetworkConfig) (speakers chan *Speaker) {
+	return getDevices(conf, true)
+}
+
+func SearchDevices(conf NetworkConfig) (speakers chan *Speaker) {
+	return getDevices(conf, false)
+}
+
+// GetDevices starts listening on the indicated interface for the speakers to listen for.
+// passes to speakers the series of speakers that are handled for further processing
+func getDevices(conf NetworkConfig, closeChannel bool) (speakers chan *Speaker) {
+	iff, err := net.InterfaceByName(conf.InterfaceName)
+
+	if err != nil {
+		log.Fatalf("Error with interface. %s", err)
+	}
+
+	for _, value := range conf.SpeakerToListenFor {
+		filteredSpeakers[value] = true
+		log.Debugf("Reacting only speakers %v\n", value)
+
+	}
+	speakers = make(chan *Speaker)
+
+	// var wg sync.WaitGroup
+	log.Debugf("Scanning for Soundtouch systems.")
+	go func() {
+		for ok := true; ok; ok = (len(visibleSpeakers) < conf.NoOfSystems) {
+			speakerCh := Lookup(iff)
+			messageCh := make(chan *Update)
+
+			for speaker := range speakerCh {
+				// LookUp found a speaker
+				speakerInfo, _ := speaker.Info()
+				speaker.DeviceInfo = speakerInfo
+				spkLogger := log.WithFields(log.Fields{
+					"Speaker": speaker.Name(),
+					"ID":      speaker.DeviceID(),
+				})
+
+				if contains(visibleSpeakers, speaker.DeviceID()) {
+					spkLogger.Debugf("Already included. Ignoring.")
+					continue
+				}
+
+				// check whether we might have to ignore the speaker
+				if len(filteredSpeakers) > 0 && !(filteredSpeakers)[speakerInfo.Name] {
+					// spkLogger.Traceln("Seen but ignoring messages from: ", speakerInfo.Name)
+					continue
+				}
+
+				visibleSpeakers[speaker.DeviceID()] = speaker
+				// wg.Add(1)
+				if conf.UpdateHandler != nil {
+					// register handler
+					speaker.UpdateHandler = conf.UpdateHandler
+				}
+				go func(s *Speaker, msgChan chan *Update) {
+					// defer wg.Done()
+					webSocketCh, _ := s.Listen()
+					s.webSocketCh = webSocketCh
+					s.UpdateHandler.Handle(webSocketCh, *s)
+				}(speaker, messageCh)
+
+				speakers <- speaker
+
+			}
+			if len(visibleSpeakers) < conf.NoOfSystems {
+				time.Sleep(10 * time.Second)
+			}
+		}
+
+		if closeChannel {
+			close(speakers)
+		}
+		log.Debugf("Found all Soundtouch systems. Normal Operation.")
+	}()
+	// wg.Wait()
+	return
+}
+
+func contains(list speakers, deviceID string) bool {
+	for _, ms := range list {
+		if ms.DeviceInfo.DeviceID == deviceID {
+			return true
+		}
+	}
+	return false
+}
