c85cb41aba23a99c4c95dfa4419103bc14b9bfc5
Theo Vassiliou <vassiliou@web.de>
2025-01-31 18:15:36 +0100

    test: LookupStaticSpeakers tests added

--- soundtouch_test.go
+++ soundtouch_test.go
@@ -122,3 +122,56 @@ func TestSpeaker_DeviceID(t *testing.T) {
 		})
 	}
 }
+
+// LookupStaticSpeakers returns a channel that will deliver a stream of *Speaker.
+// The *Speaker objects are created from the given list of IPs.
+// The function will filter out IPs that are not valid soundtouch speakers.
+func TestLookupStaticSpeakers(t *testing.T) {
+	tests := []struct {
+		name       string
+		speakerIPs []string
+		wantCount  int
+	}{
+		{
+			name:       "Empty IP list",
+			speakerIPs: []string{},
+			wantCount:  0,
+		},
+		{
+			name:       "Single IP",
+			speakerIPs: []string{"192.168.1.100"},
+			wantCount:  1,
+		},
+		{
+			name:       "Multiple IPs",
+			speakerIPs: []string{"192.168.1.100", "192.168.1.101", "192.168.1.102"},
+			wantCount:  3,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			speakerCh := LookupStaticSpeakers(tt.speakerIPs)
+
+			count := 0
+			for speaker := range speakerCh {
+				// check the content of the speaker
+				if speaker.IP.String() != tt.speakerIPs[count] {
+					t.Errorf("LookupStaticSpeakers() got speaker with IP %v, want %v", speaker.IP.String(), tt.speakerIPs[count-1])
+				}
+				if speaker.Port != 8090 {
+					t.Errorf("LookupStaticSpeakers() got speaker with port %v, want 8090", speaker.Port)
+				}
+
+				if speaker == nil {
+					t.Error("received nil speaker")
+				}
+				count++
+			}
+
+			if count != tt.wantCount {
+				t.Errorf("LookupStaticSpeakers() got %v speakers, want %v", count, tt.wantCount)
+			}
+		})
+	}
+}
